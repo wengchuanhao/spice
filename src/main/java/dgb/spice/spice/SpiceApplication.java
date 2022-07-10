408c90e24290e9b8f6f8f29ef3fbc94dc9c30d90
wch <1250018262@qq.com>
2022-07-10 22:06:30 +0800

    create commit

---
+++ src/main/java/dgb/spice/spice/SpiceApplication.java
@@ -0,0 +1,13 @@
+package dgb.spice.spice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiceApplication.class, args);
+    }
+
+}
